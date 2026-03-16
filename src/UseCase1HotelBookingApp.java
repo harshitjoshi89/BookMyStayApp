80400f808b70fb939b72e3abcf9147367c8a4e82
harshitjoshi89 <harshit8911@gmail.com>
2026-03-16 15:21:20 +0530

    UC1COMPLETED

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,11 @@
+public class UseCase1HotelBookingApp {
+
+    public class UseCase1HotelBookingApp {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Book My Stay App!");
+            System.out.println("Hotel Booking Management System v1.0");
+            System.out.println("System has started successfully.");
+        }
+    }
+}
